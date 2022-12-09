84b0507b0c76b3cc5353d86df04f8d409f2abddb
SeaBebop <54507045+SeaBebop@users.noreply.github.com>
2022-12-09 15:53:01 -0500

    Adding units.php
    This one produces the ajax for the SignUp page

---
+++ Laundry/PHP_HTML/Units.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+</head>
+
+<body>
+<?php
+$conn =  new mysqli('localhost',"root",'root','laundry');
+
+$sql = "SELECT User_ID FROM laundry.login Where password is NULL;";
+
+$result = $conn->query($sql);
+				if ($result->num_rows > 0) {
+				// output data of each row
+                echo '<div class="Units"><span class="apt-unit">Apartment Unit:</span>';
+                echo '<select name="unit" id="units" class="select-button">';
+                  echo '<option disabled selected value><span style="color:white">select unit</span></option>';
+				while($row = $result->fetch_assoc()) {
+					echo "<option value='". $row["User_ID"]. "'>". $row["User_ID"]."</option>";
+					//This appends all the data into one array
+					}
+					 echo '</select></div>';
+                echo '<input type="submit" value="Sign up" onmouseover="showAgreement();">';
+				}
+$conn->close();
+?>
+
+</body>
+
+
+
+</html>
